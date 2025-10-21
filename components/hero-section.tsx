9497b346f21192220cbe8c759a08a4cdda1e3b46
Rishi's <148547530+rishirai13@users.noreply.github.com>
2025-10-21 18:49:14 +0530

    Replace hero image with new image file

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -101,7 +101,7 @@ export default function HeroSection() {
             <div className="relative">
               <div className="w-80 h-80 lg:w-96 lg:h-96 rounded-full overflow-hidden shadow-2xl ring-8 ring-white">
                 <Image
-                  src="/images/hero-image.jpg"
+                  src="/images/hero.png"
                   alt="Rishi Rai - Full Stack Developer"
                   width={400}
                   height={400}
